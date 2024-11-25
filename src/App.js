9a728786376f3b25af1b4a0da7726bf7356eb8f1
Saroj Basnet <sarojbasnet@Sarojs-MacBook-Pro.local>
2024-11-25 16:51:03 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-
-// import './App.css';
-// import Header from './components/Header';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header></Header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// App.js
 import React from "react";
 import Header from "./components/Header";
 import HeroSection from "./components/Hero";
